d5af7b125f126ec409559c32d230c13b37dea2e7
Charlie Root <root@clonosbld141.my.domain>
2024-07-02 17:16:15 +0300

    wip

--- share/hooks.d/post.d/fio-hook-example.sh
+++ share/hooks.d/post.d/fio-hook-example.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
-echo "PROFILE: ${PROFILE}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "BS: ${BS}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "DEPTH: ${DEPTH}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "JOBS: ${JOBS}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "RWMIXREAD: ${RWMIXREAD}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "FS: ${FS}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "R_MB: ${R_MB}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "R_KB: ${R_KB}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "W_MB: ${W_MB}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "W_KB: ${W_KB}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "R_OPS: ${R_IOPS}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "R_IOPS_MAX: ${R_IOPS_MAX}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "R_IOPS_MIN: ${R_IOPS_MIN}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "W_IOPS: ${W_IOPS}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "W_IOPS_MAX: ${W_IOPS_MAX}" | tee -a /tmp/hooks.${PROFILE}.txt
-echo "W_IOPS_MIN: ${W_IOPS_MIN}" | tee -a /tmp/hooks.${PROFILE}.txt
+
+# save result into custom file in /tmp/fio directory:
+[ ! -d /tmp/fio ] && mkdir /tmp/fio
+cat > /tmp/fio/${PROFILE}-${DEVICE}.txt <<EOF
+DEVICE: ${DEVICE}
+PROFILE: ${PROFILE}
+BS: ${BS}
+DEPTH: ${DEPTH}
+JOBS: ${JOBS}
+RWMIXREAD: ${RWMIXREAD}
+FS: ${FS}
+R_MB: ${R_MB}
+R_KB: ${R_KB}
+W_MB: ${W_MB}
+W_KB: ${W_KB}
+R_IOPS: ${R_IOPS}
+R_IOPS_MAX: ${R_IOPS_MAX}
+R_IOPS_MIN: ${R_IOPS_MIN}
+W_IOPS: ${W_IOPS}
+W_IOPS_MAX: ${W_IOPS_MAX}
+W_IOPS_MIN: ${W_IOPS_MIN}
+EOF
+
+# show file content
+cat /tmp/fio/${PROFILE}-${DEVICE}.txt
