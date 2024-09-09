c1fbc072cd79c39e235144afe5408143dce5814c
Charlie Root <root@mother.olevole.ru>
2024-09-09 21:34:08 +0300

    wip

--- tests/enable_token.sh
+++ tests/enable_token.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s "https://perf.beeru.online/scripts/enable_token.php?key=SDFrDgl0U6UEpTJuw9zURwQFTSe8bHMplY4fPVxkdU%3&token=233123c4ee3d4235109f8cc921a098c9aecc819fcbcd11481502d37206852045"
+curl -s "https://fio.beeru.online/scripts/enable_token.php?key=SDFrDgl0U6UEpTJuw9zURwQFTSe8bHMplY4fPVxkdU%3&token=233123c4ee3d4235109f8cc921a098c9aecc819fcbcd11481502d37206852045"
--- tests/report_system.sh
+++ tests/report_system.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s "https://perf.beeru.online/scripts/report_system.php?key=SDFrDgl0U6UEpTJuw9zURwQFTSe8bHMplY4fPVxkdU%3&token=233123c4ee3d4235109f8cc921a098c9aecc819fcbcd11481502d37206852045&rel=14.0-CURRENT&arch=amd64&opsys=FreeBSD"
+curl -s "https://fio.beeru.online/scripts/report_system.php?key=SDFrDgl0U6UEpTJuw9zURwQFTSe8bHMplY4fPVxkdU%3&token=233123c4ee3d4235109f8cc921a098c9aecc819fcbcd11481502d37206852045&rel=14.0-CURRENT&arch=amd64&opsys=FreeBSD"
