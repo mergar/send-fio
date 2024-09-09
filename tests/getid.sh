4595ecc6a5186697a9db0b0059903fe1ec91bbbe
Charlie Root <root@mother.olevole.ru>
2024-09-09 19:57:25 +0300

    wip

--- tests/getid.sh
+++ tests/getid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s -i "https://perf.spacevm.ru/scripts/getid.php?key=SDFrDgl0U6UEpTJuw9zURwQFTSe8bHMplY4fPVxkdU%3"
+curl -s -i "https://perf.beeru.online/scripts/getid.php?key=SDFrDgl0U6UEpTJuw9zURwQFTSe8bHMplY4fPVxkdU%3"
